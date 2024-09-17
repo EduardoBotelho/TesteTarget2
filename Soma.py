cee2d5febea6fac9c7f1b5c1bb1c239de44975fd
Eduardo Botelho <67869787+EduardoBotelho@users.noreply.github.com>
2024-09-17 11:27:16 -0300

    Add files via upload

---
+++ Soma.py
@@ -0,0 +1,13 @@
+def main():
+    INDICE = 13
+    SOMA = 0
+    K = 0
+
+    while K < INDICE:
+        K = K + 1
+        SOMA = SOMA + K
+
+    print(SOMA)
+
+if __name__ == "__main__":
+    main()
